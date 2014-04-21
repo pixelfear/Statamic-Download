8d51bed2c3cf0d8132837b713b6384300467f1e7
Jason Varga <jason@pixelfear.com>
2014-04-21 19:27:18 -0400

    Don't lowercase 'as' param

--- pi.download.php
+++ pi.download.php
@@ -5,7 +5,7 @@ class Plugin_download extends Plugin
 	public function index()
 	{
 		$file = $this->fetchParam(array('file', 'filename', 'url'), null, null, false, false);
-		$as   = $this->fetchParam('as');
+		$as   = $this->fetchParam('as', null, null, false, false);
 
 		$url  = '/TRIGGER/download/download?file='.$file;
 		if ($as) {
