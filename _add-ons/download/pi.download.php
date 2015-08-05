6d7d279348c7c8a584f24c8b8cb91b77aa940f0a
Erin Dalzell <erin@thedalzells.org>
2015-08-05 09:33:42 -0700

    Add override to plugin

--- _add-ons/download/pi.download.php
+++ _add-ons/download/pi.download.php
@@ -7,6 +7,7 @@ class Plugin_download extends Plugin
 		$file 	   = $this->fetchParam(array('file', 'filename', 'url'), null, null, false, false);
 		$as   	   = $this->fetchParam('as', null, null, false, false);
 		$logged_in = $this->fetchParam('logged_in');
+		$override  = $this->fetchParam('override');
 
 		$url = URL::prependSiteRoot('/TRIGGER/download/download?file='.$file);
 		
@@ -14,8 +15,8 @@ class Plugin_download extends Plugin
 			$url .= '&as='.$as;
 		}
 		
-		if ($logged_in) {
-			$url .= '&logged_in='.$logged_in;
+		if (!$logged_in && $override) {
+			$url .= '&logged_in='.$logged_in.'&override='.$override;
 		}
 		
 		return $url;
